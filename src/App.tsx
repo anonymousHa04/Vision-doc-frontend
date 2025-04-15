e3698ede17a50dd7097b033d0c07f9acfb5e5333
anonymousHa04 <harsh97814@gmail.com>
2025-04-16 01:03:43 +0530

    setted up debugger

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello from React + Vite + TS</h1>;
+    const age = 3;
+    const multiple = 2 * age;
+  return <h1>Hello from React + Vite + TS {multiple}</h1>;
 }
 
 export default App;
