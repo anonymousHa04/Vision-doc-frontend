2573ec220fb6b65be02258ae4d78ab9f9b80acbb
anonymousHa04 <harsh97814@gmail.com>
2025-04-26 14:45:13 +0530

    fixed styling for main buttons

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
 
   return (
     <>
-      <button
+      {/* <button
         className='bg-blue-500 
         text-white 
         px-4 py-2 
@@ -45,7 +45,7 @@ function App() {
         onClick={handleButtonClick}
       >
         Button to change theme
-      </button>
+      </button> */}
 
       <Home />
       
--- src/components/mainContent/MainContent.tsx
+++ src/components/mainContent/MainContent.tsx
@@ -3,10 +3,10 @@ import { mainButtonStyle } from "../../styles/tailwindCss";
 const MainContent = ({ sidebarOpen }: { sidebarOpen: boolean }) => {
     return (
         <main
-            className={`flex-1 transition-all duration-300 ease-in-out ${sidebarOpen ? "ml-0 md:ml-0" : ""
-                } p-6 bg-gray-50 dark:bg-zinc-800`}
+            className={`flex-1 flex items-center justify-center flex-col transition-all duration-300 ease-in-out ${sidebarOpen ? "ml-0 md:ml-0" : ""
+                } bg-gray-50 dark:bg-zinc-800`}
         >
-            <h1 className="text-2xl font-bold dark:text-white">What can I Do for you ?</h1>
+            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold dark:text-white">What can I Do for you ?</h1>
 
             {/* place action buttons here */}
             <div className="mt-4 flex gap-4">
